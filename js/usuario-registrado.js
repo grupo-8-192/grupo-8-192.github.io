295543ae0a8d34ef3d8d98c0b3ee6cbb93a3ae84
nachososa <satrianoignacio@gmail.com>
2023-04-14 20:41:02 -0300

    Modificamos el css del  "Visor" de la calculadora

--- js/usuario-registrado.js
+++ js/usuario-registrado.js
@@ -1,12 +1,9 @@
 function registrarse() {
     let nombre = (document.getElementById("nombre").value);
     let apellido = (document.getElementById("apellido").value);
-    let edad = (document.getElementById("edad").value);
     let email = (document.getElementById("email").value);
-    let telefono = (document.getElementById)("telefono").value;
+    let telefono = (document.getElementById("telefono").value);
     let Provincia = (document.getElementById("Provincia").value);
-
     document.getElementById("nuevo-usuario").innerHTML = `Hola ${nombre} ${apellido} de ${Provincia} <br>
-    tu email es ${email}`;
-
+    tu email es ${email} y tu teléfono es ${telefono}`;
 }
